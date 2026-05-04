c77bd524b4ee235f7ee64d4142a4e91058be0427
duongvh <vuhaiduongit@gmail.com>
2026-05-04 13:24:42 +0700

    move button action get out of each line table to on the table,add code delete and handle button disable when click selected record

--- src/layout_admin/user_management/UserManagementLayout.js
+++ src/layout_admin/user_management/UserManagementLayout.js
@@ -3,6 +3,7 @@ import { selectListApi, createApi, selectAllRolesApi, updateApi, deleteApi } fro
 import { useDispatch, useSelector } from "react-redux";
 import { Pagination } from 'antd';
 import 'antd/dist/reset.css';
+import '../.././App.css';
 
 function UserManagement() {
   // State for modal visibility
@@ -75,6 +76,42 @@ function UserManagement() {
     }
   };
 
+  //Handle case when change size list selected user
+  useEffect(() => {
+    handleEnableButtonActions();
+  }, [selectedUsers]);
+  //Handle case when click button edit or delete but no user selected
+  const handleEnableButtonActions = () => {
+    if(selectedUsers != null &&  selectedUsers.size === 0){
+    //Disable edit and delete button when no user selected
+      disableButtonEditDelete(true, true);
+    } else if(selectedUsers != null && selectedUsers.size === 1){
+    //Enable edit button and disable delete button when only 1 user selected
+      disableButtonEditDelete(false, false);
+    }else{
+    //Disable edit and enable delete button when multiple users selected
+      disableButtonEditDelete(true, false);
+    }
+
+  };
+  const disableButtonEditDelete = (statusEdit, statusDelete) => {
+    //Set disabled attribute for edit and delete button
+    document.getElementById("edit-user-button").disabled = statusEdit;
+    document.getElementById("delete-user-button").disabled = statusDelete;
+    //Add css for disabled button edit
+    if(statusEdit){
+    document.getElementById("edit-user-button").classList.add("button-disabled");
+    }else{
+    document.getElementById("edit-user-button").classList.remove("button-disabled");
+    }
+    //Add css for disabled button delete
+    if(statusDelete){
+    document.getElementById("delete-user-button").classList.add("button-disabled");
+    } else {
+    document.getElementById("delete-user-button").classList.remove("button-disabled");
+    }
+  };
+
   // Handler for individual row checkbox
   const handleUserCheckboxChange = (userId) => {
     setSelectedUsers((prevSelected) => {
@@ -123,6 +160,7 @@ function UserManagement() {
         addButton.removeEventListener('click', handleButtonClick);
       }
     };
+    disableButtonEditDelete(true, true); // Initially disable edit and delete buttons
   }, []); // Empty dependency array means this runs once on mount
 
   useEffect(() => {
@@ -150,17 +188,21 @@ function UserManagement() {
   };
 
   // Handler for opening edit modal with user data
-  const handleOpenEditModal = (user) => {
-    setFormDataEdit({
-      id: user?.id || '',
-      username: user?.username || '',
-      email: user?.email || '',
-      phone: user?.phone || '',
-      fullName: user?.fullName || '',
-      identityCard: user?.identityCard || null,
-      address: user?.address || null,
-      note: user?.note || null,
-      roles: user?.roles?.id || '',
+  const handleOpenEditModal = () => {
+    listDataUser.forEach(user => {
+      if (user.id === selectedUsers[0]) {
+        setFormDataEdit({
+          id: user?.id || '',
+          username: user?.username || '',
+          email: user?.email || '',
+          phone: user?.phone || '',
+          fullName: user?.fullName || '',
+          identityCard: user?.identityCard || null,
+          address: user?.address || null,
+          note: user?.note || null,
+          roles: user?.roles?.id || '',
+        });
+      }
     });
     setIsEditModalOpen(true);
   };
@@ -223,6 +265,26 @@ function UserManagement() {
       if (response.type.endsWith('/fulfilled')) {
         console.log("update successful:", response.payload);
         // Store token and user info
+      } else {
+        console.error("update failed:", response.payload);
+      }
+    } catch (error) {
+      console.error("update error:", error);
+    }
+  };
+
+  //Handle for update user API call 
+  const handleDeleteUser = async () => {
+    try {
+      const response = await dispatch(deleteApi({ listData: Array.from(selectedUsers)
+       }));
+      // Check if login was successful
+      if (response.type.endsWith('/fulfilled')) {
+        console.log("update successful:", response.payload);
+        // set timeout to ensure the update API call completes before refreshing the list
+      setTimeout(() => {
+      handleSelectListUsers(pager.pageNum, pager.pageSize); // Refresh user list after creation
+      }, 500);
       } else {
         console.error("update failed:", response.payload);
       }
@@ -335,47 +397,13 @@ function UserManagement() {
           <div className="sm:flex">
             <div className="items-center hidden mb-3 sm:flex sm:divide-x sm:mb-0 dark:divide-gray-700">
               <form className="lg:pr-3" action="#" method="GET">
-                <label htmlFor="users-search" className="sr-only">Search</label>
                 <div className="relative mt-1 lg:w-64 xl:w-96">
+                <label htmlFor="users-search">Email</label>
                   <input type="text" name="email" id="users-search"
                     className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
-                    placeholder="Search for users"/>
+                    placeholder="Tìm kiếm email"/>
                 </div>
               </form>
-              <div className="flex pl-0 mt-3 space-x-1 sm:pl-2 sm:mt-0">
-                <a href="#"
-                  className="inline-flex justify-center p-1 text-gray-500 rounded cursor-pointer hover:text-gray-900 hover:bg-gray-100 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
-                  <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                    <path fillRule="evenodd"
-                      d="M11.49 3.17c-.38-1.56-2.6-1.56-2.98 0a1.532 1.532 0 01-2.286.948c-1.372-.836-2.942.734-2.106 2.106.54.886.061 2.042-.947 2.287-1.561.379-1.561 2.6 0 2.978a1.532 1.532 0 01.947 2.287c-.836 1.372.734 2.942 2.106 2.106a1.532 1.532 0 012.287.947c.379 1.561 2.6 1.561 2.978 0a1.533 1.533 0 012.287-.947c1.372.836 2.942-.734 2.106-2.106a1.533 1.533 0 01.947-2.287c1.561-.379 1.561-2.6 0-2.978a1.532 1.532 0 01-.947-2.287c.836-1.372-.734-2.942-2.106-2.106a1.532 1.532 0 01-2.287-.947zM10 13a3 3 0 100-6 3 3 0 000 6z"
-                      clipRule="evenodd"></path>
-                  </svg>
-                </a>
-                <a href="#"
-                  className="inline-flex justify-center p-1 text-gray-500 rounded cursor-pointer hover:text-gray-900 hover:bg-gray-100 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
-                  <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                    <path fillRule="evenodd"
-                      d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
-                      clipRule="evenodd"></path>
-                  </svg>
-                </a>
-                <a href="#"
-                  className="inline-flex justify-center p-1 text-gray-500 rounded cursor-pointer hover:text-gray-900 hover:bg-gray-100 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
-                  <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                    <path fillRule="evenodd"
-                      d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7 4a1 1 0 11-2 0 1 1 0 012 0zm-1-9a1 1 0 00-1 1v4a1 1 0 102 0V6a1 1 0 00-1-1z"
-                      clipRule="evenodd"></path>
-                  </svg>
-                </a>
-                <a href="#"
-                  className="inline-flex justify-center p-1 text-gray-500 rounded cursor-pointer hover:text-gray-900 hover:bg-gray-100 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
-                  <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                    <path
-                      d="M10 6a2 2 0 110-4 2 2 0 010 4zM10 12a2 2 0 110-4 2 2 0 010 4zM10 18a2 2 0 110-4 2 2 0 010 4z">
-                    </path>
-                  </svg>
-                </a>
-              </div>
             </div>
           </div>
           <div className="sm:flex">   
@@ -411,6 +439,34 @@ function UserManagement() {
                 </svg>
                 Xuất exel
               </a>
+              <button
+                type="button"
+                id="edit-user-button"
+                onClick={handleOpenEditModal} // Use state handler instead of data attributes
+                className="inline-flex items-center justify-center w-1/2 px-3 py-2 text-sm font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 sm:w-auto dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+              >
+                <svg className="w-5 h-5 mr-2 -ml-1" fill="currentColor" viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg">
+                  <path fillRule="evenodd"
+                    d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z"
+                    clipRule="evenodd"></path>
+                </svg>
+                Sửa
+              </button>
+              <button
+                type="button"
+                id="delete-user-button"
+                onClick={handleDeleteUser} // Use state handler instead of data attributes
+                className="inline-flex items-center justify-center w-1/2 px-3 py-2 text-sm font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 sm:w-auto dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+              >
+                <svg className="w-5 h-5 mr-2 -ml-1" fill="currentColor" viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg">
+                  <path fillRule="evenodd"
+                    d="M6 2a2 2 0 00-2 2v12a2 2 0 002 2h8a2 2 0 002-2V7.414A2 2 0 0015.414 6L12 2.586A2 2 0 0010.586 2H6zm5 6a1 1 0 10-2 0v3.586l-1.293-1.293a1 1 0 10-1.414 1.414l3 3a1 1 0 001.414 0l3-3a1 1 0 00-1.414-1.414L11 11.586V8z"
+                    clipRule="evenodd"></path>
+                </svg>
+                Xóa
+              </button>
             </div>
           </div>
                    
@@ -445,6 +501,10 @@ function UserManagement() {
                     </th>
                     <th scope="col"
                       className="p-4 text-xs font-medium text-left text-gray-500 uppercase dark:text-gray-400">
+                      Họ và tên
+                    </th>
+                    <th scope="col"
+                      className="p-4 text-xs font-medium text-left text-gray-500 uppercase dark:text-gray-400">
                       Email
                     </th>
                     <th scope="col"
@@ -459,10 +519,6 @@ function UserManagement() {
                       className="p-4 text-xs font-medium text-left text-gray-500 uppercase dark:text-gray-400">
                       Trạng thái
                     </th>
-                    <th scope="col"
-                      className="p-4 text-xs font-medium text-left text-gray-500 uppercase dark:text-gray-400">
-                      Hành động
-                    </th>
                   </tr>
                 </thead>
                 <tbody className="bg-white divide-y divide-gray-200 dark:bg-gray-800 dark:divide-gray-700">
@@ -475,6 +531,7 @@ function UserManagement() {
                       const phoneNumber = user?.phone;
                       const identityCardUser = user?.identityCard;
                       const activeStatus = user?.status === '1' || user?.status === 1 || user?.status === true;
+                      const fullName = user?.fullName;
 
                       return (
                         <tr key={userId} className="hover:bg-gray-100 dark:hover:bg-gray-700">
@@ -500,6 +557,9 @@ function UserManagement() {
                             </div>
                           </td> */}
                           <td className="max-w-sm p-4 overflow-hidden text-base font-normal text-gray-500 truncate xl:max-w-xs dark:text-gray-400">
+                            {fullName}
+                          </td>
+                          <td className="max-w-sm p-4 overflow-hidden text-base font-normal text-gray-500 truncate xl:max-w-xs dark:text-gray-400">
                             {userEmail}
                           </td>
                           <td className="max-w-sm p-4 overflow-hidden text-base font-normal text-gray-500 truncate xl:max-w-xs dark:text-gray-400">
@@ -513,23 +573,6 @@ function UserManagement() {
                               <div className={`h-2.5 w-2.5 rounded-full ${activeStatus ? 'bg-green-400' : 'bg-red-500'} mr-2`} />
                               <span>{activeStatus ? 'Hoạt động' : 'Không hoạt động'}</span>
                             </div>
-                          </td>
-                          <td className="p-4 space-x-2 whitespace-nowrap">
-                            <button type="button" onClick={() => handleOpenEditModal(user)}
-                              className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">
-                              <svg className="w-4 h-4 mr-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                                <path d="M17.414 2.586a2 2 0 00-2.828 0L7 10.172V13h2.828l7.586-7.586a2 2 0 000-2.828z" />
-                                <path fillRule="evenodd" d="M2 6a2 2 0 012-2h4a1 1 0 010 2H4v10h10v-4a1 1 0 112 0v4a2 2 0 01-2 2H4a2 2 0 01-2-2V6z" clipRule="evenodd" />
-                              </svg>Sửa
-                            </button>
-                            <button type="button" onClick={openDeleteModal}
-                              className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-red-600 rounded-lg hover:bg-red-800 focus:ring-4 focus:ring-red-300 dark:focus:ring-red-900">
-                              <svg className="w-4 h-4 mr-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                                <path fillRule="evenodd"
-                                  d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
-                                  clipRule="evenodd"></path>
-                              </svg>Xóa
-                            </button>
                           </td>
                         </tr>
                       );
@@ -791,20 +834,18 @@ function UserManagement() {
               <div className="p-6 pt-0 text-center">
                 <svg className="w-16 h-16 mx-auto text-red-600" fill="none" stroke="currentColor" viewBox="0 0 24 24"
                   xmlns="http://www.w3.org/2000/svg">
-                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
                     d="M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
                 </svg>
-                <h3 className="mt-5 mb-6 text-lg text-gray-500 dark:text-gray-400">Are you sure you want to delete this
-                  user?</h3>
-                <a href="#"
+                <h3 className="mt-5 mb-6 text-lg text-gray-500 dark:text-gray-400">Bạn có chắc chắn xóa người dùng ngày?</h3>
+                <a onClick={handleDeleteUser}
                   className="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-base inline-flex items-center px-3 py-2.5 text-center mr-2 dark:focus:ring-red-800">
-                  Yes, I'm sure
+                  Chắc chắn
                 </a>
-                <a href="#"
-                  onClick={closeDeleteModal} // Changed to state handler
+                <a onClick={closeDeleteModal} // Changed to state handler
                   className="text-gray-900 bg-white hover:bg-gray-100 focus:ring-4 focus:ring-primary-300 border border-gray-200 font-medium inline-flex items-center rounded-lg text-base px-3 py-2.5 text-center dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700 dark:focus:ring-gray-700"
                   data-modal-hide="delete-user-modal">
-                  No, cancel
+                  Không, hủy bỏ
                 </a>
               </div>
             </div>
